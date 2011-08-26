1e3b1f90123d04e7f6cdcef9526183b5ded94a06
thomas.jungblut <thomas.jungblut@googlemail.com>
2011-08-26 13:15:43 +0000

    reads a unified solution file

--- antcolony/src/de/jungblut/antcolony/SolutionReader.java
+++ antcolony/src/de/jungblut/antcolony/SolutionReader.java
@@ -15,28 +15,18 @@ public class SolutionReader {
 	 * @throws IOException
 	 * @throws NumberFormatException
 	 */
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		final BufferedReader br = new BufferedReader(new FileReader(new File("files/berlin52.tsp")));
+	public static void main(String[] args) throws NumberFormatException,
+			IOException {
+		final BufferedReader br = new BufferedReader(new FileReader(new File(
+				"files/my.tsp")));
 
 		final ArrayList<Record> records = new ArrayList<Record>();
 
-		boolean readAhead = false;
 		String line;
 		while ((line = br.readLine()) != null) {
-
-			if (line.equals("EOF")) {
-				break;
-			}
-
-			if (readAhead) {
-				String[] split = line.split(" ");
-				records.add(new AntColonyOptimization.Record(Double.parseDouble(split[1]), Double
-						.parseDouble(split[2])));
-			}
-
-			if (line.equals("NODE_COORD_SECTION")) {
-				readAhead = true;
-			}
+			String[] split = line.split(" ");
+			records.add(new AntColonyOptimization.Record(Double
+					.parseDouble(split[0]), Double.parseDouble(split[1])));
 		}
 
 		br.close();
@@ -47,17 +37,19 @@ public class SolutionReader {
 			Record h = records.get(i + 1);
 			distance += calculateEuclidianDistance(r.x, r.y, h.x, h.y);
 		}
-		
-		Record r = records.get(records.size()-1);
+
+		Record r = records.get(records.size() - 1);
 		Record h = records.get(0);
 		distance += calculateEuclidianDistance(r.x, r.y, h.x, h.y);
-		
+
 		System.out.println("Optimal distance is: " + distance);
 	}
 
-	private static final double calculateEuclidianDistance(double x1, double y1, double x2,
-			double y2) {
-		return (Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
+	private static final double calculateEuclidianDistance(double x1,
+			double y1, double x2, double y2) {
+		double d = (Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
+		System.out.println(x1 + "/" + y1 + " : " + x2 + "/" + y2 + " = " + d);
+		return d;
 	}
 
 }
