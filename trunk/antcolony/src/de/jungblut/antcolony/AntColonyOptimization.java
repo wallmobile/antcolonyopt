a8bf1d1082e27e3959b54476e7a17b8792ab36fe
thomas.jungblut@googlemail.com <thomas.jungblut@googlemail.com@ddb85f09-5667-2f3b-209a-63b92bbd8fd6>
2012-10-13 13:54:27 +0000

    better formatting, few simple fixes

--- trunk/antcolony/src/de/jungblut/antcolony/AntColonyOptimization.java
+++ trunk/antcolony/src/de/jungblut/antcolony/AntColonyOptimization.java
@@ -15,264 +15,267 @@ import cern.jet.random.Uniform;
 
 public final class AntColonyOptimization {
 
-	private static String TSP_FILE = "files/berlin52.tsp";
-	// greedy
-	public static final double ALPHA = -0.2d;
-	// rapid selection
-	public static final double BETA = 9.6d;
+  private static String TSP_FILE = "files/berlin52.tsp";
+  // greedy
+  public static final double ALPHA = -0.2d;
+  // rapid selection
+  public static final double BETA = 9.6d;
 
-	// heuristic parameters
-	public static final double Q = 0.0001d; // somewhere between 0 and 1
-	public static final double PHEROMONE_PERSISTENCE = 0.3d; // between 0 and 1
-	public static final double INITIAL_PHEROMONES = 0.8d; // can be anything
+  // heuristic parameters
+  public static final double Q = 0.0001d; // somewhere between 0 and 1
+  public static final double PHEROMONE_PERSISTENCE = 0.3d; // between 0 and 1
+  public static final double INITIAL_PHEROMONES = 0.8d; // can be anything
 
-	// use power of 2
-	public static final int numOfAgents = 2048 * 20;
-	private static final int poolSize = Runtime.getRuntime().availableProcessors();
+  // use power of 2
+  public static final int NUM_AGENTS = 2048 * 20;
+  private static final int POOL_SIZE = Runtime.getRuntime()
+      .availableProcessors();
 
-	private Uniform uniform;
+  private Uniform uniform;
 
-	private final ExecutorService threadPool = Executors.newFixedThreadPool(poolSize);
+  private static final ExecutorService THREAD_POOL = Executors
+      .newFixedThreadPool(POOL_SIZE);
 
-	private final ExecutorCompletionService<WalkedWay> agentCompletionService = new ExecutorCompletionService<WalkedWay>(
-			threadPool);
+  private final ExecutorCompletionService<WalkedWay> agentCompletionService = new ExecutorCompletionService<WalkedWay>(
+      THREAD_POOL);
 
-	final double[][] matrix;
-	final double[][] invertedMatrix;
-	private final double[][] pheromones;
-	private final Object[][] mutexes;
+  final double[][] matrix;
+  final double[][] invertedMatrix;
 
-	public AntColonyOptimization() throws IOException {
-		// read the matrix
-		matrix = readMatrixFromFile();
-		invertedMatrix = invertMatrix();
-		pheromones = initializePheromones();
-		mutexes = initializeMutexObjects();
-		// (double min, double max, int seed)
-		uniform = new Uniform(0, matrix.length - 1, (int) System.currentTimeMillis());
-	}
+  private final double[][] pheromones;
+  private final Object[][] mutexes;
 
-	private final Object[][] initializeMutexObjects() {
-		final Object[][] localMatrix = new Object[matrix.length][matrix.length];
-		int rows = matrix.length;
-		for (int columns = 0; columns < matrix.length; columns++) {
-			for (int i = 0; i < rows; i++) {
-				localMatrix[columns][i] = new Object();
-			}
-		}
+  public AntColonyOptimization() throws IOException {
+    // read the matrix
+    matrix = readMatrixFromFile();
+    invertedMatrix = invertMatrix();
+    pheromones = initializePheromones();
+    mutexes = initializeMutexObjects();
+    // (double min, double max, int seed)
+    uniform = new Uniform(0, matrix.length - 1,
+        (int) System.currentTimeMillis());
+  }
 
-		return localMatrix;
-	}
+  private final Object[][] initializeMutexObjects() {
+    final Object[][] localMatrix = new Object[matrix.length][matrix.length];
+    int rows = matrix.length;
+    for (int columns = 0; columns < matrix.length; columns++) {
+      for (int i = 0; i < rows; i++) {
+        localMatrix[columns][i] = new Object();
+      }
+    }
 
-	final double readPheromone(int x, int y) {
-		return pheromones[x][y];
-	}
+    return localMatrix;
+  }
 
-	final void adjustPheromone(int x, int y, double newPheromone) {
-		synchronized (mutexes[x][y]) {
-			final double result = calculatePheromones(pheromones[x][y], newPheromone);
-			if (result >= 0.0d) {
-				pheromones[x][y] = result;
-			} else {
-				pheromones[x][y] = 0;
-			}
-		}
-	}
+  final double readPheromone(int x, int y) {
+    return pheromones[x][y];
+  }
 
-	private final double calculatePheromones(double current, double newPheromone) {
-		final double result = (1 - AntColonyOptimization.PHEROMONE_PERSISTENCE) * current
-				+ newPheromone;
-		return result;
-	}
+  final void adjustPheromone(int x, int y, double newPheromone) {
+    synchronized (mutexes[x][y]) {
+      final double result = calculatePheromones(pheromones[x][y], newPheromone);
+      if (result >= 0.0d) {
+        pheromones[x][y] = result;
+      } else {
+        pheromones[x][y] = 0;
+      }
+    }
+  }
 
-	final void adjustPheromone(int[] way, double newPheromone) {
-		synchronized (pheromones) {
-			for (int i = 0; i < way.length - 1; i++) {
-				pheromones[way[i]][way[i + 1]] = calculatePheromones(
-						pheromones[way[i]][way[i + 1]], newPheromone);
-			}
-			pheromones[way[way.length - 1]][way[0]] = calculatePheromones(
-					pheromones[way.length - 1][way[0]], newPheromone);
-		}
-	}
+  private final double calculatePheromones(double current, double newPheromone) {
+    final double result = (1 - AntColonyOptimization.PHEROMONE_PERSISTENCE)
+        * current + newPheromone;
+    return result;
+  }
 
-	private final double[][] initializePheromones() {
-		final double[][] localMatrix = new double[matrix.length][matrix.length];
-		int rows = matrix.length;
-		for (int columns = 0; columns < matrix.length; columns++) {
-			for (int i = 0; i < rows; i++) {
-				localMatrix[columns][i] = INITIAL_PHEROMONES;
-			}
-		}
+  private final double[][] initializePheromones() {
+    final double[][] localMatrix = new double[matrix.length][matrix.length];
+    int rows = matrix.length;
+    for (int columns = 0; columns < matrix.length; columns++) {
+      for (int i = 0; i < rows; i++) {
+        localMatrix[columns][i] = INITIAL_PHEROMONES;
+      }
+    }
 
-		return localMatrix;
-	}
+    return localMatrix;
+  }
 
-	private final double[][] readMatrixFromFile() throws IOException {
+  private final double[][] readMatrixFromFile() throws IOException {
 
-		final BufferedReader br = new BufferedReader(new FileReader(new File(TSP_FILE)));
+    final BufferedReader br = new BufferedReader(new FileReader(new File(
+        TSP_FILE)));
 
-		final LinkedList<Record> records = new LinkedList<Record>();
+    final LinkedList<Record> records = new LinkedList<Record>();
 
-		boolean readAhead = false;
-		String line;
-		while ((line = br.readLine()) != null) {
+    boolean readAhead = false;
+    String line;
+    while ((line = br.readLine()) != null) {
 
-			if (line.equals("EOF")) {
-				break;
-			}
+      if (line.equals("EOF")) {
+        break;
+      }
 
-			if (readAhead) {
-				String[] split = sweepNumbers(line.trim());
-				records.add(new Record(Double.parseDouble(split[1].trim()), Double
-						.parseDouble(split[2].trim())));
-			}
+      if (readAhead) {
+        String[] split = sweepNumbers(line.trim());
+        records.add(new Record(Double.parseDouble(split[1].trim()), Double
+            .parseDouble(split[2].trim())));
+      }
 
-			if (line.equals("NODE_COORD_SECTION")) {
-				readAhead = true;
-			}
-		}
+      if (line.equals("NODE_COORD_SECTION")) {
+        readAhead = true;
+      }
+    }
 
-		br.close();
+    br.close();
 
-		final double[][] localMatrix = new double[records.size()][records.size()];
+    final double[][] localMatrix = new double[records.size()][records.size()];
 
-		int rIndex = 0;
-		for (Record r : records) {
-			int hIndex = 0;
-			for (Record h : records) {
-				localMatrix[rIndex][hIndex] = calculateEuclidianDistance(r.x, r.y, h.x, h.y);
-				hIndex++;
-			}
-			rIndex++;
-		}
+    int rIndex = 0;
+    for (Record r : records) {
+      int hIndex = 0;
+      for (Record h : records) {
+        localMatrix[rIndex][hIndex] = calculateEuclidianDistance(r.x, r.y, h.x,
+            h.y);
+        hIndex++;
+      }
+      rIndex++;
+    }
 
-		return localMatrix;
-	}
+    return localMatrix;
+  }
 
-	private final String[] sweepNumbers(String trim) {
-		String[] arr = new String[3];
-		int currentIndex = 0;
-		for (int i = 0; i < trim.length(); i++) {
-			final char c = trim.charAt(i);
-			if (((int) c) != 32) {
-				for (int f = i + 1; f < trim.length(); f++) {
-					final char x = trim.charAt(f);
-					if (((int) x) == 32) {
-						arr[currentIndex] = trim.substring(i, f);
-						currentIndex++;
-						break;
-					} else if(f == trim.length() - 1){
-						arr[currentIndex] = trim.substring(i, trim.length());
-						break;
-					}
-				}
-				i = i + arr[currentIndex-1].length();
-			}
-		}
-		return arr;
-	}
+  private final String[] sweepNumbers(String trim) {
+    String[] arr = new String[3];
+    int currentIndex = 0;
+    for (int i = 0; i < trim.length(); i++) {
+      final char c = trim.charAt(i);
+      if ((c) != 32) {
+        for (int f = i + 1; f < trim.length(); f++) {
+          final char x = trim.charAt(f);
+          if ((x) == 32) {
+            arr[currentIndex] = trim.substring(i, f);
+            currentIndex++;
+            break;
+          } else if (f == trim.length() - 1) {
+            arr[currentIndex] = trim.substring(i, trim.length());
+            break;
+          }
+        }
+        i = i + arr[currentIndex - 1].length();
+      }
+    }
+    return arr;
+  }
 
-	private final double[][] invertMatrix() {
-		double[][] local = new double[matrix.length][matrix.length];
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix.length; j++) {
-				local[i][j] = invertDouble(matrix[i][j]);
-			}
-		}
-		return local;
-	}
+  private final double[][] invertMatrix() {
+    double[][] local = new double[matrix.length][matrix.length];
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix.length; j++) {
+        local[i][j] = invertDouble(matrix[i][j]);
+      }
+    }
+    return local;
+  }
 
-	private final double invertDouble(double distance) {
-		if (distance == 0)
-			return 0;
-		else
-			return 1.0d / distance;
-	}
+  private final double invertDouble(double distance) {
+    if (distance == 0d)
+      return 0d;
+    else
+      return 1.0d / distance;
+  }
 
-	private final double calculateEuclidianDistance(double x1, double y1, double x2, double y2) {
-		return Math.abs((Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))));
-	}
+  private final double calculateEuclidianDistance(double x1, double y1,
+      double x2, double y2) {
+    final double xDiff = x2 - x1;
+    final double yDiff = y2 - y1;
+    return Math.abs((Math.sqrt((xDiff * xDiff) + (yDiff * yDiff))));
+  }
 
-	final double start() throws InterruptedException, ExecutionException {
+  final double start() throws InterruptedException, ExecutionException {
 
-		WalkedWay bestDistance = null;
+    WalkedWay bestDistance = null;
 
-		int agentsSend = 0;
-		int agentsDone = 0;
-		int agentsWorking = 0;
-		for (int agentNumber = 0; agentNumber < numOfAgents; agentNumber++) {
-			agentCompletionService.submit(new Agent(this, getGaussianDistributionRowIndex()));
-			agentsSend++;
-			agentsWorking++;
-			while (agentsWorking >= poolSize) {
-				WalkedWay way = agentCompletionService.take().get();
-				if (bestDistance == null || way.distance < bestDistance.distance) {
-					bestDistance = way;
-					System.out.println("Agent returned with new best distance of: " + way.distance);
-				}
-				agentsDone++;
-				agentsWorking--;
-			}
-		}
-		final int left = agentsSend - agentsDone;
-		System.out.println("Waiting for " + left + " agents to finish their random walk!");
+    int agentsSend = 0;
+    int agentsDone = 0;
+    int agentsWorking = 0;
+    for (int agentNumber = 0; agentNumber < NUM_AGENTS; agentNumber++) {
+      agentCompletionService.submit(new Agent(this,
+          getGaussianDistributionRowIndex()));
+      agentsSend++;
+      agentsWorking++;
+      while (agentsWorking >= POOL_SIZE) {
+        WalkedWay way = agentCompletionService.take().get();
+        if (bestDistance == null || way.distance < bestDistance.distance) {
+          bestDistance = way;
+          System.out.println("Agent returned with new best distance of: "
+              + way.distance);
+        }
+        agentsDone++;
+        agentsWorking--;
+      }
+    }
+    final int left = agentsSend - agentsDone;
+    System.out.println("Waiting for " + left
+        + " agents to finish their random walk!");
 
-		for (int i = 0; i < left; i++) {
-			WalkedWay way = agentCompletionService.take().get();
-			if (bestDistance == null || way.distance < bestDistance.distance) {
-				bestDistance = way;
-				System.out.println("Agent returned with new best distance of: " + way.distance);
-			}
-		}
+    for (int i = 0; i < left; i++) {
+      WalkedWay way = agentCompletionService.take().get();
+      if (bestDistance == null || way.distance < bestDistance.distance) {
+        bestDistance = way;
+        System.out.println("Agent returned with new best distance of: "
+            + way.distance);
+      }
+    }
 
-		threadPool.shutdownNow();
-		System.out.println("Found best so far: " + bestDistance.distance);
-		System.out.println(Arrays.toString(bestDistance.way));
+    THREAD_POOL.shutdownNow();
+    System.out.println("Found best so far: " + bestDistance.distance);
+    System.out.println(Arrays.toString(bestDistance.way));
 
-		return bestDistance.distance;
+    return bestDistance.distance;
 
-	}
+  }
 
-	private final int getGaussianDistributionRowIndex() {
-		return uniform.nextInt();
-	}
+  private final int getGaussianDistributionRowIndex() {
+    return uniform.nextInt();
+  }
 
-	static class Record {
-		double x;
-		double y;
+  static class Record {
+    double x;
+    double y;
 
-		public Record(double x, double y) {
-			super();
-			this.x = x;
-			this.y = y;
-		}
-	}
+    public Record(double x, double y) {
+      super();
+      this.x = x;
+      this.y = y;
+    }
+  }
 
-	static class WalkedWay {
-		int[] way;
-		double distance;
+  static class WalkedWay {
+    int[] way;
+    double distance;
 
-		public WalkedWay(int[] way, double distance) {
-			super();
-			this.way = way;
-			this.distance = distance;
-		}
-	}
+    public WalkedWay(int[] way, double distance) {
+      super();
+      this.way = way;
+      this.distance = distance;
+    }
+  }
 
-	public static void main(String[] args) throws IOException, InterruptedException,
-			ExecutionException {
+  public static void main(String[] args) throws IOException,
+      InterruptedException, ExecutionException {
 
-		if (args.length > 0) {
-			AntColonyOptimization.TSP_FILE = args[0];
-			System.out.println("Using " + args[0]);
-		}
+    if (args.length > 0) {
+      AntColonyOptimization.TSP_FILE = args[0];
+      System.out.println("Using " + args[0]);
+    }
 
-		long start = System.currentTimeMillis();
-		AntColonyOptimization antColonyOptimization = new AntColonyOptimization();
-		double result = antColonyOptimization.start();
-		System.out.println("Took: " + (System.currentTimeMillis() - start) + " ms!");
-		System.out.println("Result was: " + result);
-	}
+    long start = System.currentTimeMillis();
+    AntColonyOptimization antColonyOptimization = new AntColonyOptimization();
+    double result = antColonyOptimization.start();
+    System.out
+        .println("Took: " + (System.currentTimeMillis() - start) + " ms!");
+    System.out.println("Result was: " + result);
+  }
 
 }
--- trunk/antcolony/src/de/jungblut/antcolony/ParameterTesting.java
+++ trunk/antcolony/src/de/jungblut/antcolony/ParameterTesting.java
@@ -9,158 +9,158 @@ import java.util.concurrent.ExecutionException;
  */
 public class ParameterTesting {
 
-	static TreeSet<TestRecord> alphaQueue;
+  static TreeSet<TestRecord> alphaQueue;
 
-	public static void main(String[] args) throws IOException,
-			InterruptedException, ExecutionException {
+  public static void main(String[] args) throws IOException,
+      InterruptedException, ExecutionException {
 
-		alphatesting();
-		betatesting();
-		// initPheroTest();
-	}
+    alphatesting();
+    betatesting();
+    // initPheroTest();
+  }
 
-	public static void alphatesting() throws IOException, InterruptedException,
-			ExecutionException {
+  public static void alphatesting() throws IOException, InterruptedException,
+      ExecutionException {
 
-		TreeSet<TestRecord> queue = new TreeSet<ParameterTesting.TestRecord>();
+    TreeSet<TestRecord> queue = new TreeSet<ParameterTesting.TestRecord>();
 
-		for (double i = -10.0d; i <= 10.0d; i += 0.1d) {
-			double[] avg = new double[5];
-			for (int times = 0; times < 5; times++) {
-				System.out.println("Testing: " + i);
-				AntColonyOptimization opt = new AntColonyOptimization();
-				// AntColonyOptimization.ALPHA = i;
-				final double result = opt.start();
-				avg[times] = result;
-			}
+    for (double i = -10.0d; i <= 10.0d; i += 0.1d) {
+      double[] avg = new double[5];
+      for (int times = 0; times < 5; times++) {
+        System.out.println("Testing: " + i);
+        AntColonyOptimization opt = new AntColonyOptimization();
+        // AntColonyOptimization.ALPHA = i;
+        final double result = opt.start();
+        avg[times] = result;
+      }
 
-			double best = Double.MAX_VALUE;
-			double sum = 0.0;
-			for (double d : avg) {
-				sum += d;
-				if (best > d)
-					best = d;
-			}
+      double best = Double.MAX_VALUE;
+      double sum = 0.0;
+      for (double d : avg) {
+        sum += d;
+        if (best > d)
+          best = d;
+      }
 
-			double average = sum / avg.length;
-			queue.add(new TestRecord(i, average, best));
+      double average = sum / avg.length;
+      queue.add(new TestRecord(i, average, best));
 
-			System.out.println("Best alpha found until now was: "
-					+ queue.first().toString());
-		}
+      System.out.println("Best alpha found until now was: "
+          + queue.first().toString());
+    }
 
-		System.out.println("Best alpha found was: " + queue.first().toString());
-		if (queue.size() > 5) {
-			TreeSet<TestRecord> temp = new TreeSet<ParameterTesting.TestRecord>();
-			for (TestRecord t : queue) {
-				temp.add(t);
-			}
-			queue = temp;
-		}
-		System.out.println(queue.toString());
-		alphaQueue = queue;
-	}
+    System.out.println("Best alpha found was: " + queue.first().toString());
+    if (queue.size() > 5) {
+      TreeSet<TestRecord> temp = new TreeSet<ParameterTesting.TestRecord>();
+      for (TestRecord t : queue) {
+        temp.add(t);
+      }
+      queue = temp;
+    }
+    System.out.println(queue.toString());
+    alphaQueue = queue;
+  }
 
-	public static void betatesting() throws IOException, InterruptedException,
-			ExecutionException {
+  public static void betatesting() throws IOException, InterruptedException,
+      ExecutionException {
 
-		TreeSet<TestRecord> queue = new TreeSet<ParameterTesting.TestRecord>();
+    TreeSet<TestRecord> queue = new TreeSet<ParameterTesting.TestRecord>();
 
-		for (TestRecord r : alphaQueue) {
-			for (double i = 0d; i <= 10.0d; i += 0.1d) {
-				double[] avg = new double[5];
-				for (int times = 0; times < 5; times++) {
-					System.out.println("Testing: " + i);
-					AntColonyOptimization opt = new AntColonyOptimization();
-					// AntColonyOptimization.ALPHA = r.parameter;
-					// AntColonyOptimization.BETA = i;
-					double start = opt.start();
-					avg[times] = start;
-				}
+    for (TestRecord r : alphaQueue) {
+      for (double i = 0d; i <= 10.0d; i += 0.1d) {
+        double[] avg = new double[5];
+        for (int times = 0; times < 5; times++) {
+          System.out.println("Testing: " + i);
+          AntColonyOptimization opt = new AntColonyOptimization();
+          // AntColonyOptimization.ALPHA = r.parameter;
+          // AntColonyOptimization.BETA = i;
+          double start = opt.start();
+          avg[times] = start;
+        }
 
-				double best = Double.MAX_VALUE;
-				double sum = 0.0;
-				for (double d : avg) {
-					sum += d;
-					if (best > d)
-						best = d;
-				}
+        double best = Double.MAX_VALUE;
+        double sum = 0.0;
+        for (double d : avg) {
+          sum += d;
+          if (best > d)
+            best = d;
+        }
 
-				double average = sum / avg.length;
-				queue.add(new TestRecord(i, average, best, r));
+        double average = sum / avg.length;
+        queue.add(new TestRecord(i, average, best, r));
 
-				System.out.println("Best BETA found until now was: "
-						+ queue.first().toString());
+        System.out.println("Best BETA found until now was: "
+            + queue.first().toString());
 
-			}
-		}
+      }
+    }
 
-		System.out.println("Best BETA found was: " + queue.first().toString());
-		if (queue.size() > 5) {
-			TreeSet<TestRecord> temp = new TreeSet<ParameterTesting.TestRecord>();
-			for (TestRecord t : queue) {
-				temp.add(t);
-			}
-			queue = temp;
-		}
-		System.out.println(queue.toString());
-		alphaQueue = queue;
-	}
+    System.out.println("Best BETA found was: " + queue.first().toString());
+    if (queue.size() > 5) {
+      TreeSet<TestRecord> temp = new TreeSet<ParameterTesting.TestRecord>();
+      for (TestRecord t : queue) {
+        temp.add(t);
+      }
+      queue = temp;
+    }
+    System.out.println(queue.toString());
+    alphaQueue = queue;
+  }
 
-	public static void testingPhero() throws IOException, InterruptedException,
-			ExecutionException {
-		double bestAlpha = -10.0d;
-		double bestResult = Double.MAX_VALUE;
-		for (double i = 0d; i <= 10.0d; i += 0.1d) {
-			System.out.println("Testing: " + i);
-			AntColonyOptimization opt = new AntColonyOptimization();
-			// AntColonyOptimization.PHEROMONE_PERSISTENCE = i;
-			double result = opt.start();
-			if (result < bestResult) {
-				bestAlpha = i;
-				bestResult = result;
-			}
-			System.out.println("Best phero found was: " + bestAlpha);
-		}
+  public static void testingPhero() throws IOException, InterruptedException,
+      ExecutionException {
+    double bestAlpha = -10.0d;
+    double bestResult = Double.MAX_VALUE;
+    for (double i = 0d; i <= 10.0d; i += 0.1d) {
+      System.out.println("Testing: " + i);
+      AntColonyOptimization opt = new AntColonyOptimization();
+      // AntColonyOptimization.PHEROMONE_PERSISTENCE = i;
+      double result = opt.start();
+      if (result < bestResult) {
+        bestAlpha = i;
+        bestResult = result;
+      }
+      System.out.println("Best phero found was: " + bestAlpha);
+    }
 
-		System.out.println("Best phero found was: " + bestAlpha);
-	}
+    System.out.println("Best phero found was: " + bestAlpha);
+  }
 
-	static class TestRecord implements Comparable<TestRecord> {
-		double parameter;
-		double avg;
-		double best;
+  static class TestRecord implements Comparable<TestRecord> {
+    double parameter;
+    double avg;
+    double best;
 
-		TestRecord nestedAlpha;
+    TestRecord nestedAlpha;
 
-		public TestRecord(double parameter, double avg, double best) {
-			super();
-			this.parameter = parameter;
-			this.avg = avg;
-			this.best = best;
-		}
+    public TestRecord(double parameter, double avg, double best) {
+      super();
+      this.parameter = parameter;
+      this.avg = avg;
+      this.best = best;
+    }
 
-		public TestRecord(double parameter, double avg, double best,
-				TestRecord nestedAlpha) {
-			super();
-			this.parameter = parameter;
-			this.avg = avg;
-			this.best = best;
-			this.nestedAlpha = nestedAlpha;
-		}
+    public TestRecord(double parameter, double avg, double best,
+        TestRecord nestedAlpha) {
+      super();
+      this.parameter = parameter;
+      this.avg = avg;
+      this.best = best;
+      this.nestedAlpha = nestedAlpha;
+    }
 
-		// TODO test with best and avg
-		@Override
-		public int compareTo(TestRecord o) {
-			return Double.compare(best, o.best);
-		}
+    // TODO test with best and avg
+    @Override
+    public int compareTo(TestRecord o) {
+      return Double.compare(best, o.best);
+    }
 
-		@Override
-		public String toString() {
-			return "[parameter=" + parameter + ", avg=" + avg + ", best="
-					+ best + ", nestedAlpha=" + nestedAlpha + "]";
-		}
+    @Override
+    public String toString() {
+      return "[parameter=" + parameter + ", avg=" + avg + ", best=" + best
+          + ", nestedAlpha=" + nestedAlpha + "]";
+    }
 
-	}
+  }
 
 }
